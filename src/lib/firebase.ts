34bfb8472e24e8e07791783fb9cd32086182772c
kaka12136465 <programing11661166@gmail.com>
2026-01-18 14:33:38 +0900

    fix: 誤ってAPI KeyをpushしていたのでAPI鍵の削除、変更、非公開化

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,13 +5,12 @@ import { getStorage } from 'firebase/storage';
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBcefWV-uYaSv6cSe7Uxyr7B5HYyU1ENJw",
-  authDomain: "englishwordlearning-d636b.firebaseapp.com",
-  projectId: "englishwordlearning-d636b",
-  storageBucket: "englishwordlearning-d636b.firebasestorage.app",
-  messagingSenderId: "1013792565434",
-  appId: "1:1013792565434:web:08da58bb8962bdbcef3a83",
-  measurementId: "G-SB9EHLH8KC"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
