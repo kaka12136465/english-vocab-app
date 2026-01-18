239ade1eff71224bf8c1c908e56e1ca428abb9c4
kaka12136465 <programing11661166@gmail.com>
2026-01-18 14:18:26 +0900

    feat: WPA化

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <meta name="mobile-web-app-capable" content="yes"></meta>
     <App />
   </React.StrictMode>,
 )
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,37 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    VitePWA({
+      registerType: 'autoUpdate',  // ← これを確認
+      workbox: {
+        cleanupOutdatedCaches: true,  // 追加
+        skipWaiting: true,             // 追加
+        clientsClaim: true             // 追加
+      },
+      manifest: {
+      name: 'English Vocab App',
+      short_name: 'Vocab',
+      description: '英単語学習アプリ',
+      theme_color: '#ffffff',
+      icons: [
+        {
+          src: 'pwa-192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+      ]
+    }
+    })
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-    },
+    }
   },
+  
 })
